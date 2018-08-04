25f3af0d788a657615eda184838ab794b8332343
Slendercoder <andradeed78@gmail.com>
2018-08-04 12:43:21 -0500

    Incluye funcion que encuentra distnacia minima a focal pahts

--- DCLAttractors.py
+++ DCLAttractors.py
@@ -2,6 +2,79 @@ print "loading packages..."
 import RandomPath as rp
 import ExploreGrid as eg
 
+
+# ------------------------------------------
+# Functions
+# ------------------------------------------
+def set_distance(l1, l2):
+    # Finds the distance between lists l1 and l2
+    # Lists l1 and l2 must contain only 0 and 1
+    # print "Finding distance between "
+    # print l1
+    # print "and"
+    # print l2
+
+    intersect = np.sum([np.where(l1[i] + l2[i] > 1, 1, 0) for i in range(0, Num_Loc * Num_Loc)])
+    # print "Intersection: " + str(intersect)
+    uni = np.sum([np.where(l1[i] + l2[i] > 0, 1, 0) for i in range(0, Num_Loc * Num_Loc)])
+    # print "Union: " + str(uni)
+    if uni == 0:
+        return 0
+    else:
+        return 1 - (float(intersect)/uni)
+
+def find_min_distance_2_Focal(path):
+
+    print "Finding distances to focal paths..."
+
+    size = Num_Loc * Num_Loc
+    half_size = Num_Loc * Num_Loc / 2
+    half_Num_Loc = Num_Loc / 2
+
+    # First pair of complementary paths -- UD
+    UP = [1] * half_size + [0] * half_size
+    DOWN = [1 - i for i in UP]
+    # Second pair of complementary paths -- LR
+    RIGHT = []
+    for i in range(0, Num_Loc):
+        RIGHT += [0] * half_Num_Loc + [1] * half_Num_Loc
+
+    LEFT = [1 - i for i in RIGHT]
+    # Third pair of complementary paths -- AN
+    ALL = [1] * size
+    NOTHING = [0] * size
+    # Fourth pair of complementary paths -- IO
+    IN = []
+    for i in range(Num_Loc):
+        IN += [0] + [1] * (Num_Loc - 2) + [0]
+
+    OUT = [1 - i for i in IN]
+
+    # print "Finding distances to each focal path..."
+    D_2_UP = set_distance(path, UP)
+    D_2_DOWN = set_distance(path, DOWN)
+    D_2_LEFT = set_distance(path, LEFT)
+    D_2_RIGHT = set_distance(path, RIGHT)
+    D_2_ALL = set_distance(path, ALL)
+    D_2_NOTHING = set_distance(path, NOTHING)
+    D_2_IN = set_distance(path, IN)
+    D_2_OUT = set_distance(path, OUT)
+    # print "D_2_UP: " + str(D_2_UP)
+    # print "D_2_DOWN: " + str(D_2_DOWN)
+    # print "D_2_LEFT: " + str(D_2_LEFT)
+    # print "D_2_RIGHT: " + str(D_2_RIGHT)
+    # print "D_2_ALL: " + str(D_2_ALL)
+    # print "D_2_NOTHING: " + str(D_2_NOTHING)
+    # print "D_2_IN: " + str(D_2_IN)
+    # print "D_2_OUT: " + str(D_2_OUT)
+    minimo = min([\
+                D_2_UP, D_2_DOWN, \
+                D_2_LEFT, D_2_RIGHT, \
+                D_2_ALL, D_2_NOTHING, \
+                D_2_IN, D_2_OUT, \
+                ])
+    return minimo
+
 # ------------------------------------------
 # Parameters
 # ------------------------------------------
