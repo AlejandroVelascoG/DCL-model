39b2064a8548ccfc5c38e586e531c690692c0e13
Slendercoder <andradeed78@gmail.com>
2018-08-08 11:26:57 -0500

    Version funcionando

--- DCLAttractors.py
+++ DCLAttractors.py
@@ -1,95 +1,10 @@
-print "loading packages..."
-import RandomPath as rp
-import ExploreGrid as eg
-import Analyze as al
+# Code for the model of division of cognitive labor
+# with focal pahts.
+# Edgar Andrade, Miguel Valencia, Juan Camilo Hoyos
+# 2018
 
-# ------------------------------------------
-# Functions
-# ------------------------------------------
-def set_distance(l1, l2):
-    # Finds the distance between lists l1 and l2
-    # Lists l1 and l2 must contain only 0 and 1
-
-    intersect = np.sum([np.where(l1[i] + l2[i] > 1, 1, 0) for i in range(0, Num_Loc * Num_Loc)])
-    # print "Intersection: " + str(intersect)
-    uni = np.sum([np.where(l1[i] + l2[i] > 0, 1, 0) for i in range(0, Num_Loc * Num_Loc)])
-    # print "Union: " + str(uni)
-    if uni == 0:
-        return 0
-    else:
-        return 1 - (float(intersect)/uni)
-
-def find_min_distance_2_Focal(path):
-    '''
-    Finds the minimum distance from a path to a focal path
-
-    Input: path
-    Output: minimum distance to a focal path,
-            closest focal path
-    '''
-
-    print "Finding distances to focal paths..."
-
-    size = Num_Loc * Num_Loc
-    half_size = Num_Loc * Num_Loc / 2
-    half_Num_Loc = Num_Loc / 2
-
-    # First pair of complementary paths -- UD
-    UP = [1] * half_size + [0] * half_size
-    DOWN = [1 - i for i in UP]
-    # Second pair of complementary paths -- LR
-    RIGHT = []
-    for i in range(0, Num_Loc):
-        RIGHT += [0] * half_Num_Loc + [1] * half_Num_Loc
-
-    LEFT = [1 - i for i in RIGHT]
-    # Third pair of complementary paths -- AN
-    ALL = [1] * size
-    NOTHING = [0] * size
-    # Fourth pair of complementary paths -- IO
-    IN = []
-    for i in range(Num_Loc):
-        IN += [0] + [1] * (Num_Loc - 2) + [0]
-
-    OUT = [1 - i for i in IN]
-
-    # print "Finding distances to each focal path..."
-    D_2_UP = set_distance(path, UP)
-    D_2_DOWN = set_distance(path, DOWN)
-    D_2_LEFT = set_distance(path, LEFT)
-    D_2_RIGHT = set_distance(path, RIGHT)
-    D_2_ALL = set_distance(path, ALL)
-    D_2_NOTHING = set_distance(path, NOTHING)
-    D_2_IN = set_distance(path, IN)
-    D_2_OUT = set_distance(path, OUT)
-    # print "D_2_UP: " + str(D_2_UP)
-    # print "D_2_DOWN: " + str(D_2_DOWN)
-    # print "D_2_LEFT: " + str(D_2_LEFT)
-    # print "D_2_RIGHT: " + str(D_2_RIGHT)
-    # print "D_2_ALL: " + str(D_2_ALL)
-    # print "D_2_NOTHING: " + str(D_2_NOTHING)
-    # print "D_2_IN: " + str(D_2_IN)
-    # print "D_2_OUT: " + str(D_2_OUT)
-
-    distances = {}
-
-    distances[D_2_UP] = UP
-    distances[D_2_DOWN] = DOWN
-    distances[D_2_LEFT] = LEFT
-    distances[D_2_RIGHT] = RIGHT
-    distances[D_2_ALL] = ALL
-    distances[D_2_NOTHING] = NOTHING
-    distances[D_2_IN] = IN
-    distances[D_2_OUT] = OUT
-
-    minimo = min([\
-                D_2_UP, D_2_DOWN, \
-                D_2_LEFT, D_2_RIGHT, \
-                D_2_ALL, D_2_NOTHING, \
-                D_2_IN, D_2_OUT, \
-                ])
-
-    return minimo, distances[minimo]
+import focalPaths as fp
+from random import uniform
 
 # ------------------------------------------
 # Parameters
@@ -99,9 +14,10 @@ p = 0.5 # probability of there being a unicorn
 Pl = 2 # number of players
 Num_Loc = 8 # number of locations (squares in a row in the grid)
 SIZE = [1]*64
-numIter = 1
-Tolerance = 32
+numIter = 60
+Tolerance = 30
 Stubornness = 3
+Threshold = 0.1
 # ------------------------------------------
 # Here begins the action
 # ------------------------------------------
@@ -109,14 +25,13 @@ Stubornness = 3
 # Creates the players
 Players = []
 for k in range(0, Pl):
-	Players.append(eg.player(\
+	Players.append(fp.player(\
                              False,\
                              "",\
-                             rp.RandomPath(SIZE),\
+                             fp.RandomPath(SIZE),\
+                             int(uniform(0, 1000)), \
                              [],\
-                             0, \
                              False, \
-                             int(uniform(0, 1000000)), \
                              0\
                              )\
                     )
@@ -139,19 +54,21 @@ countB = 0 # Initializes couter for Stubornness
 
 for i in range(numIter):
 
+    regions = fp.ExploreGrid(p, Num_Loc, Players,f, i)
+
     for k in range(len(Players)):
 
-        regions = eg.ExploreGrid(p, Num_Loc, Players,f, numIter)
+        Minimo, camino = fp.find_min_distance_2_Focal(Players[k].path, Num_Loc)
 
-        if find_min_distance_2_Focal(Players[k].path) == 0:
+        if Minimo == 0:
             if (Players[k].score < Tolerance):
                 countB += 1
                 if countB > Stubornness:
-                    Players[k].path = rp.RandomPath(SIZE)
+                    Players[k].path = fp.RandomPath(SIZE)
 
         else:
-            Path = al.Analize(regions[k])
-            Minimo, camino = find_min_distance_2_Focal(Path)
+            Path = fp.analyze(regions[k], Threshold, SIZE, Num_Loc)
+            Minimo, camino = fp.find_min_distance_2_Focal(Path, Num_Loc)
             if Minimo == 0:
                 Players[k].path = camino
                 countB = 0
--- focalPaths.py
+++ focalPaths.py
@@ -1,12 +1,16 @@
+# Objects and functions for the model of division of cognitive labor
+# with focal pahts.
+# Edgar Andrade, Miguel Valencia, Juan Camilo Hoyos
+# 2018
+
 print "loading packages..."
-from random import choice, uniform, random, sample, randint, shuffle
+from random import uniform, random, sample, shuffle
 from math import floor
 import numpy as np
-from RandomPath import RandomPath
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
-
+# ------------------------------------------
+# Objects
+# ------------------------------------------
 # Define de players
 class player(object):
 	'''Object defining a player. Has the following properties:
@@ -19,6 +23,141 @@ class player(object):
 		self.where = Where # Tiles actually visited
 		self.accuracy = Accuracy # True or False: Whether the player's decision is correct
 		self.score = Score # Player's score
+
+
+
+# ------------------------------------------
+# Functions
+# ------------------------------------------
+
+def set_distance(l1, l2, Num_Loc):
+    # Finds the distance between lists l1 and l2
+    # Lists l1 and l2 must contain only 0 and 1
+	# Num_Loc is the number of rows in the grid
+
+    intersect = np.sum([np.where(l1[i] + l2[i] > 1, 1, 0) for i in range(0, Num_Loc * Num_Loc)])
+    # print "Intersection: " + str(intersect)
+    uni = np.sum([np.where(l1[i] + l2[i] > 0, 1, 0) for i in range(0, Num_Loc * Num_Loc)])
+    # print "Union: " + str(uni)
+    if uni == 0:
+        return 0
+    else:
+        return 1 - (float(intersect)/uni)
+
+def find_min_distance_2_Focal(path, Num_Loc):
+    '''
+    Finds the minimum distance from a path to a focal path
+
+    Input: path is a list with path followed
+		   Num_Loc is the number of rows in the grid
+
+    Output: minimum distance to a focal path,
+            closest focal path
+    '''
+
+    print "Finding distances to focal paths..."
+
+    size = Num_Loc * Num_Loc
+    half_size = Num_Loc * Num_Loc / 2
+    half_Num_Loc = Num_Loc / 2
+
+    # First pair of complementary paths -- UD
+    UP = [1] * half_size + [0] * half_size
+    DOWN = [1 - i for i in UP]
+    # Second pair of complementary paths -- LR
+    RIGHT = []
+    for i in range(0, Num_Loc):
+        RIGHT += [0] * half_Num_Loc + [1] * half_Num_Loc
+
+    LEFT = [1 - i for i in RIGHT]
+    # Third pair of complementary paths -- AN
+    ALL = [1] * size
+    NOTHING = [0] * size
+    # Fourth pair of complementary paths -- IO
+    IN = []
+    for i in range(Num_Loc):
+        IN += [0] + [1] * (Num_Loc - 2) + [0]
+
+    OUT = [1 - i for i in IN]
+
+    # print "Finding distances to each focal path..."
+    D_2_UP = set_distance(path, UP, Num_Loc)
+    D_2_DOWN = set_distance(path, DOWN, Num_Loc)
+    D_2_LEFT = set_distance(path, LEFT, Num_Loc)
+    D_2_RIGHT = set_distance(path, RIGHT, Num_Loc)
+    D_2_ALL = set_distance(path, ALL, Num_Loc)
+    D_2_NOTHING = set_distance(path, NOTHING, Num_Loc)
+    D_2_IN = set_distance(path, IN, Num_Loc)
+    D_2_OUT = set_distance(path, OUT, Num_Loc)
+    # print "D_2_UP: " + str(D_2_UP)
+    # print "D_2_DOWN: " + str(D_2_DOWN)
+    # print "D_2_LEFT: " + str(D_2_LEFT)
+    # print "D_2_RIGHT: " + str(D_2_RIGHT)
+    # print "D_2_ALL: " + str(D_2_ALL)
+    # print "D_2_NOTHING: " + str(D_2_NOTHING)
+    # print "D_2_IN: " + str(D_2_IN)
+    # print "D_2_OUT: " + str(D_2_OUT)
+
+    distances = {}
+
+    distances[D_2_UP] = UP
+    distances[D_2_DOWN] = DOWN
+    distances[D_2_LEFT] = LEFT
+    distances[D_2_RIGHT] = RIGHT
+    distances[D_2_ALL] = ALL
+    distances[D_2_NOTHING] = NOTHING
+    distances[D_2_IN] = IN
+    distances[D_2_OUT] = OUT
+
+    minimo = min([\
+                D_2_UP, D_2_DOWN, \
+                D_2_LEFT, D_2_RIGHT, \
+                D_2_ALL, D_2_NOTHING, \
+                D_2_IN, D_2_OUT, \
+                ])
+
+    return minimo, distances[minimo]
+
+def separateTiles(visitedTiles, Num_Loc):
+    '''
+    Input: visitedTiles es una lista con 0s (no visito), 1s (visito solo el jugador),
+    	   2s (ambos jugadores visitaron)
+		   Num_Loc is the number of rows in the grid
+
+    Output: soloTiles, jointTiles
+    '''
+    soloTiles = []
+    jointTiles = []
+    for i in visitedTiles:
+        if i==1:
+            soloTiles.append(1)
+            jointTiles.append(0)
+        elif i==2:
+            soloTiles.append(0)
+            jointTiles.append(1)
+        else:
+            soloTiles.append(0)
+            jointTiles.append(0)
+    return soloTiles, jointTiles
+
+def analyze(visitedTiles, Threshold, SIZE, Num_Loc):
+	'''
+	Comentarios
+
+    Input: ?????????????
+		   Num_Loc is the number of rows in the grid
+	'''
+	soloTiles, jointTiles = separateTiles(visitedTiles, Num_Loc)
+	distance, path = find_min_distance_2_Focal(soloTiles, Num_Loc)
+
+	if distance < Threshold:
+		return path
+	else:
+		distance, path = find_min_distance_2_Focal(jointTiles, Num_Loc)
+		if distance < Threshold:
+			return [1 - i for i in path]
+		else:
+			return RandomPath(SIZE)
 
 def ExploreGrid(probUnicorn, Num_Loc, Players, f, round):
     '''
@@ -293,4 +432,74 @@ def comprobarExploreGrid():
     f.close()
 
     # fig4.show()
+    plt.show()
+
+def weighted_choice(weights):
+    '''
+    Returns a number with probability determined by weights
+    Simple linear approach from
+    https://eli.thegreenplace.net/2010/01/22/weighted-random-generation-in-python
+    '''
+    totals = []
+    running_total = 0
+    for w in weights:
+        running_total += w
+        totals.append(running_total)
+    # rnd = random.random() * running_total
+    rnd = random() * running_total
+    for i, total in enumerate(totals):
+        if rnd < total:
+            return i
+
+def RandomPath(SIZE):
+    '''
+    Input: SIZE which is the distribution of number of tiles visited
+           SIZE must be a list of the frequencies/weight for each size
+
+    Output: PATH which is a random path whose number of tiles depends on SIZE
+    '''
+
+    Num_Rows = len(SIZE) ** 0.5
+    assert Num_Rows == int(Num_Rows), "Oops, problems with lenght of SIZE"
+    Num_Rows = int(Num_Rows)
+
+    grid = range(0, Num_Rows * Num_Rows)
+    n = weighted_choice(SIZE)
+    Casillas_a_visitar = sample(grid, n)
+    path = []
+    for i in range(0, Num_Rows * Num_Rows):
+        if i in Casillas_a_visitar:
+            path.append(1)
+        else:
+            path.append(0)
+
+    return path
+
+def comprobarRandomPath(SIZE, n):
+    '''
+    Input: SIZE must be a list of the frequencies/weight for each size
+              n must be the number of times to try the RandomPath function
+    Output: A histogram with the sizes obtained by repeating
+            n times the RandomPath function
+    '''
+    print "Checking correctness of RandomPath..."
+    sizesObtained = []
+    for i in range(0, n):
+        l = np.sum(RandomPath(SIZE))
+        sizesObtained.append(l)
+
+    # print sizesObtained
+
+    x = np.array(sizesObtained)
+    counts = np.bincount(x).tolist()
+    # print counts
+
+    x = range(len(SIZE))
+    width = 1/1.5
+    f, axarr = plt.subplots(1,2)
+    axarr[0].bar(x, SIZE, width, color = 'blue')
+    axarr[1].bar(x, counts, width, color = 'blue')
+    # plt.title("SIZE Histogram")
+    # plt.xlabel("Size")
+    # plt.ylabel("Frequency")
     plt.show()
